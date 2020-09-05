8cb21195ef81021e86f8894adf530b1d3632c457
Zzeongyx2 <39085743+Zzeongyx2@users.noreply.github.com>
2020-09-06 01:18:26 +0900

    HW1

---
+++ src/practice_11.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class practice_11 {
+    static int[] arr = new int[6];
+    static int[] sum = new int[6];
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("숫자 6개를 입력하세요 : ");
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println("누적합 : ");
+
+        for(int i =0; i<arr.length; i++){
+            System.out.println(SumFunction(i));
+        }
+    }
+
+    public static int SumFunction(int n){
+        if(n == 0){
+            return arr[0];
+        }
+        else{
+            sum[n] = arr[n] + SumFunction(n-1);
+        }
+        return sum[n];
+    }
+}
+
+
